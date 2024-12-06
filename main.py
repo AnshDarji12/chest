f5e9f254a110fbc59bc3620b68e64cda9fc7e8e4
Ansh Darji <anshdarji96@gmail.com>
2024-12-06 16:26:31 +0530

    upadated Model training

--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_preparebase_mdel import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage03_model_pipeline import ModelTrainingPipeline
 
 
 
-STAGE_NAME = "Prepare base model"
+
+STAGE_NAME = "Training"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   prepare_base_model = PrepareBaseModelTrainingPipeline()
-   prepare_base_model.main()
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
+
+
